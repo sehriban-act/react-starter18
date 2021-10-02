20be3f3e6d3e32fd31eaf8925a987896b0427ef7
Mark-Mad <mark.m@clarusway.com>
2021-10-02 17:03:30 +0300

    3. ders

--- javascript/class-notes/Session-03/app.js
+++ javascript/class-notes/Session-03/app.js
@@ -96,9 +96,34 @@ console.log(null+3); //3
 console.log(true > false); //true */
 
 
+console.log('false equals false, correct? ', false == false);
+console.log('Boolean(0) equals FALSE, correct? ', Boolean(0) == false);
+console.log('0 equals false, correct? ', 0 == false);
+console.log('Boolean("0") equals TRUE, correct? ', Boolean('0') == true);
+console.log('0' == false);
 
+// console.log('\n*** null boolean operations ***');
+// console.log('Boolean(null) equals to', Boolean(null)); 
+// console.log('null == false equals to', null == false);
+// console.log('null == true equals to', null == true); 
+// console.log('typeof null equals to', typeof null); 
+// console.log('0 == null equals to', 0 == null); // 
+// console.log('Boolean(null) == Boolean(0)', Boolean(null) == Boolean(0));
+ 
+/* console.log('\n*** undefined boolean operations ***');
+console.log('Boolean(undefined) equals', Boolean(undefined));
+console.log('undefined == false correct?', undefined == false);
+console.log('undefined == true correct?', undefined == true);
+console.log('0 == undefined correct?', 0 == undefined);
+console.log('null == undefined correct?', null == undefined);
+console.log('null === undefined correct?', null === undefined); */
 
-
+/* console.log('\n*** NaN operations ***');
+console.log(NaN == NaN); 
+console.log(NaN === NaN); 
+console.log(typeof NaN);
+mathhew = 'new student'; */
+// ! use global isNaN() function to overcome this
 
 
 
