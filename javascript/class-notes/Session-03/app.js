ce3eb6902fa423d3a48dc5ae1b2edfeee47d5a78
Mark-Mad <mark.m@clarusway.com>
2021-10-02 16:53:29 +0300

    2. Ders sonu

--- javascript/class-notes/Session-03/app.js
+++ javascript/class-notes/Session-03/app.js
@@ -45,8 +45,8 @@
 // d = -e
 // console.log({d},{e});
 
-var a = '10';
-var b = '2';
+// var a = '10';
+// var b = '2';
 
 // c = a + b;
 
@@ -61,8 +61,39 @@ var b = '2';
 // console.log(typeof (a-b),a-b);
 
 
+var a = '10';
+var b = '2';
+var c = 2
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(b == c); //true
+// console.log(b === c); //false
+// console.log(a > b);
 
 
+// console.log('1'.charCodeAt());
+// console.log('2'.charCodeAt());
+
+// console.log('fullStack' < 'fullstack');
+
+// console.log('A'.charCodeAt());
+// console.log('a'.charCodeAt());
+
+// 0,1,2,3,4,5,6,7 Octal sayı sistemi 
+/* console.log(015+025); //34 5+8 = 13 -- 5+16=21
+console.log(15+025); //36 15 + 21
+console.log(015+25); //38 13 + 25
+console.log(15+25); //40
+console.log(017+085); //100 */
+
+/* console.log(9 < 8 ); //false
+console.log(9 < 8 < 7 ); //true
+console.log(false < 7 ); //true
+console.log(false < -1); //false
+console.log(false > -1); //true
+console.log(null+3); //3
+console.log(true > false); //true */
 
 
 
