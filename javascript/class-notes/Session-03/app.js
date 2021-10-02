928ded4395d836ed4840bd244807f22bbc6a1781
Mark-Mad <mark.m@clarusway.com>
2021-10-02 16:14:43 +0300

    2. ders

--- javascript/class-notes/Session-03/app.js
+++ javascript/class-notes/Session-03/app.js
@@ -1,6 +1,6 @@
-var a = 10;
-var b = 2;
-var c = 3;
+// var a = 10;
+// var b = 2;
+// var c = 3;
 
 // console.log(a+b);
 // console.log(a-b);
@@ -27,12 +27,55 @@ var c = 3;
 
 // console.log(typeof d, d);
 
-var d = 8;
-var e = 7;
-var f = 2;
-console.log({d},{e},{f});
-// console.log('d += e', d += e ,{d},{e});
-// console.log('d -= e', d -= e ,{d},{e});
-// console.log('d *= e', d *= e ,{d},{e});
-// console.log('d /= f', d /= f ,{d},{f});
-// console.log('d %= f', d %= f ,{d},{f});
+// var d = 8;
+// var e = 7;
+// var f = 2;
+// console.log({d},{e},{f});
+// console.log('d += e', d += e ,{d},{e}); //15
+// console.log('d -= e', d -= e ,{d},{e}); //1
+// console.log('d *= e', d *= e ,{d},{e}); //56
+// console.log('d /= f', d /= f ,{d},{f}); //4
+// console.log('d %= f', d %= f ,{d},{f}); //0
+// console.log('d %= e', d %= e ,{d},{e}); //1
+
+// g = 10/3;
+// console.log(parseInt(g));
+// console.log(Number(g));
+
+// d = -e
+// console.log({d},{e});
+
+var a = '10';
+var b = '2';
+
+// c = a + b;
+
+// console.log(c,typeof c);
+
+// d = +c;
+
+// console.log(d,typeof d);
+
+// console.log(typeof (a/b),a/b);
+// console.log(typeof (a*b),a*b);
+// console.log(typeof (a-b),a-b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
