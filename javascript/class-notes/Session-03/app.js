e0068184d502ac8bc54f6ac04ddafad9afd94efc
Mark-Mad <mark.m@clarusway.com>
2021-10-02 15:56:48 +0300

    İlk ders sonu

---
+++ javascript/class-notes/Session-03/app.js
@@ -0,0 +1,38 @@
+var a = 10;
+var b = 2;
+var c = 3;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a % b);
+// console.log(a % c);
+// console.log(a**c)
+
+// a = --b 
+// a = b--
+// console.log({b});
+// console.log({a});
+
+
+// var c = 100+50*3
+
+// a = ++b 
+// a = b++
+// console.log({b});
+// console.log({a});
+
+// var d = a / b;
+
+// console.log(typeof d, d);
+
+var d = 8;
+var e = 7;
+var f = 2;
+console.log({d},{e},{f});
+// console.log('d += e', d += e ,{d},{e});
+// console.log('d -= e', d -= e ,{d},{e});
+// console.log('d *= e', d *= e ,{d},{e});
+// console.log('d /= f', d /= f ,{d},{f});
+// console.log('d %= f', d %= f ,{d},{f});
