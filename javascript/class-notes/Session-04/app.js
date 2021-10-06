176bfaa940012525238542c95eae09c29af5306d
Mark-Mad <mark.m@clarusway.com>
2021-10-06 21:20:57 +0300

    Leap year

--- javascript/class-notes/Session-04/app.js
+++ javascript/class-notes/Session-04/app.js
@@ -27,14 +27,14 @@ score >= 50 ? console.log(`Tebrikler notunuz ${score}, sınavdan geçtiniz`) :
 username ? console.log(`Merhaba ${username}`) : console.log('Please Login'); */
 
 
-// if (score >= 80) { 
-//     console.log(`Tebrikler notunuz ${score}, çok iyi bir öğrencisiniz`);
-// }else if (score >= 50) {
-//     console.log(`Tebrikler notunuz ${score}, sınavdan geçtiniz`);
-// }else{
+/* if (score >= 80) { 
+    console.log(`Tebrikler notunuz ${score}, çok iyi bir öğrencisiniz`);
+}else if (score >= 50) {
+    console.log(`Tebrikler notunuz ${score}, sınavdan geçtiniz`);
+}else{
     
-//     console.log(`Üzgünüm notunuz ${score}, sınavdan kaldınız`);
-// }
+    console.log(`Üzgünüm notunuz ${score}, sınavdan kaldınız`);
+} */
 
 /* let score = prompt(' Sınav notunu gir : ')
 
@@ -52,8 +52,61 @@ console.log(userName && console.log(`Merhaba ${userName}`)); */
 
 //Switch Case Yapısı
 
+/* var text;
+// var fruits ='LemoN';
+
+var fruits = prompt(" Lütfen bir meyve ismi giriniz : ")
+
+switch (fruits.toLowerCase()) {
+    case "banana":
+        text = " Banana is good!";
+        break;
+    case "lime":
+    case "lemon":
+    case "orange":
+        text = ` I am not fan of ${fruits}!`;
+        break;
+    case "apple":
+        text = "How you like them apples?";
+        break;
+    default :
+        text = `I have never heard of ${fruits}`;
+    }
+
+    console.log(text); */
 
 
+var year = 2024;
+var month = 4;
+var dayCount;
+switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        dayCount = 31;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        dayCount = 30;
+        break;
+    case 2:
+        if (((year % 4 == 0) && !(year % 100 == 0))
+            || (year % 400 == 0))
+            dayCount = 29;
+        else
+            dayCount = 28;
+        break;
+    default:
+        dayCount = -1; // invalid month
+}
+
+console.log(dayCount); // 29
 
 
 
