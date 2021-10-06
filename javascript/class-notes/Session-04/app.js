123e6239c47c92edb8f75a111b3f715ebab9bde2
Mark-Mad <mark.m@clarusway.com>
2021-10-06 20:35:03 +0300

    1. der

---
+++ javascript/class-notes/Session-04/app.js
@@ -0,0 +1,55 @@
+
+
+/* let score = 85;
+// let grade = score >= 50
+if (score >= 80) { 
+    console.log(`Tebrikler notunuz ${score}, çok iyi bir öğrencisiniz`);
+}else if (score >= 50) {
+    console.log(`Tebrikler notunuz ${score}, sınavdan geçtiniz`);
+}else{
+    
+    console.log(`Üzgünüm notunuz ${score}, sınavdan kaldınız`);
+}
+
+// console.log('ders bitti'); */
+
+
+// Ternary If else
+
+/* let score = 49;
+
+score >= 50 ? console.log(`Tebrikler notunuz ${score}, sınavdan geçtiniz`) : 
+               console.log(`Üzgünüm notunuz ${score}, sınavdan kaldınız`); */
+
+
+/* let username = 'Mark';
+
+username ? console.log(`Merhaba ${username}`) : console.log('Please Login'); */
+
+
+// if (score >= 80) { 
+//     console.log(`Tebrikler notunuz ${score}, çok iyi bir öğrencisiniz`);
+// }else if (score >= 50) {
+//     console.log(`Tebrikler notunuz ${score}, sınavdan geçtiniz`);
+// }else{
+    
+//     console.log(`Üzgünüm notunuz ${score}, sınavdan kaldınız`);
+// }
+
+/* let score = prompt(' Sınav notunu gir : ')
+
+score >= 80 ? console.log(`Tebrikler notunuz ${score}, çok iyi bir öğrencisiniz`) : 
+   (score >= 50 ? console.log(`Tebrikler notunuz ${score}, sınavdan geçtiniz`): 
+   console.log(`Üzgünüm notunuz ${score}, sınavdan kaldınız`))
+
+ */
+
+
+let username
+
+
+
+
+
+
+
