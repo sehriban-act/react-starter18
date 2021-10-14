29ca10dc3301d413ed852371709df547a8ac83c1
Mark-Mad <mark.m@clarusway.com>
2021-10-14 21:39:37 +0300

    2. ders

---
+++ javascript/class-notes/Session-07/array.js
@@ -0,0 +1,56 @@
+/* const car = ['Opel', 'Audi', 'BMW', 2, ['Hello','World']];
+// console.log(car);
+// console.log(car[4][0]);
+
+const car2 = new Array ('Opel', 'Audi', 'BMW', 2, ['Hello','World']); */
+
+// console.log(car2);
+
+// const x = new Array (2,10)
+// const x = new Array (10)
+// console.log(x);
+
+/* const car = ['Opel', 'Audi', 'BMW', 2, ['Hello','World']];
+
+car[4] = 'Mercedes'
+
+// console.log(car);
+console.log(typeof car);
+console.log(car instanceof Array);
+console.log(Array.isArray(car));
+ */
+
+//length
+
+/* const cars = ['Opel', 'Audi', 'BMW'];
+
+// console.log(cars);
+const cars3 = ['Opel', 'Audi', 'BMW', 2, ['Hello','World']];
+//concat()
+const cars2 = ['Mercedes', 'Renault']
+
+console.log(cars2.concat(cars,'cars3'));
+console.log(''+cars2);
+ */
+
+//sort()
+/* const daltones = ['Joe','Jack','William','Avarel']
+// daltones.sort()
+// console.log(daltones)
+daltones.reverse()
+console.log(daltones) */
+
+/* const points = [40, 100, 1, 5, 25, 10];
+
+console.log(points.sort());
+console.log(points.sort(function(a, b){return a - b})); */
+
+/* var alphabet = 'abcçdefgğhıijklmnoöprsştuüvyz0123456789'
+
+function ascii(text) {
+    for(let i=0;i < text.length;i++ ){
+    console.log(text[i],text.charCodeAt(i))
+}
+}
+// ascii(alphabet.toLocaleUpperCase('tr-TR'))
+ascii(alphabet.toLocaleUpperCase()) */
