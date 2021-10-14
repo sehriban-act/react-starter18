f80c35b99f9161b5c4bd1f78e43b6d26ffd7ce54
Mark-Mad <mark.m@clarusway.com>
2021-10-14 22:47:47 +0300

    3. ders sonu

--- javascript/class-notes/Session-07/array.js
+++ javascript/class-notes/Session-07/array.js
@@ -54,3 +54,101 @@ function ascii(text) {
 }
 // ascii(alphabet.toLocaleUpperCase('tr-TR'))
 ascii(alphabet.toLocaleUpperCase()) */
+
+//Push method
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.push("Pears", 'fig');
+// console.log(fruits);
+
+// let c = fruits.pop();
+// console.log(c);
+// console.log(fruits);
+
+// fruits.pop();
+
+
+// shift () & unshift()
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+/* const deleted = fruits.shift()
+console.log(fruits)
+console.log(deleted) */
+
+/* const newLength = fruits.unshift('Pineapple', 'Fig' )
+console.log(fruits)
+console.log(newLength) */
+
+/* const fruits = ["Banana", ,"Orange", "Apple", "Mango"];
+console.log(fruits);
+// console.log(fruits[1]);
+fruits[100] = 'abc'
+console.log(fruits.length);
+console.log(fruits);
+ */
+
+//splice ()
+
+// var names = ["John", "Edward", "Victor"]
+// var deleted = names.splice (1,2, "Mark", "James")
+// var deleted = names.splice (1,2, "Mark", "James")
+// console.log(deleted);
+// console.log(names);
+
+// names.splice(-2,1)
+// console.log(names);
+/* names.splice(-2)
+names.splice(1)
+console.log(names); */
+
+//slice()
+
+/* const months = ['Jan', 'March', 'April', 'June',"july"];
+// const springs = months.slice(1,3);
+// const springs = months.slice(-4,-2);
+// const springs = months.slice(1,-2);
+const springs = months.slice(-2,-5); //çalışmaz boş array döndürür
+console.log(springs);
+console.log(months);
+ */
+
+// const colors = ["Red", "Green", "Yellow", "Blue", "Pink", "Green"];
+
+// const x = colors.indexOf("Green", 3)
+// const x1 = colors.lastIndexOf("Green", 3)
+// console.log(x);
+// console.log(x1);
+// console.log(colors.indexOf('Black'));
+// console.log(colors.lastIndexOf('Black'));
+
+
+//iterate methods
+const colors = ["Red", "Yellow", "Green", , "Blue", "Pink","Purple","Gray","lime"];
+
+
+// 1. for 
+// for (let i = 0; i < colors.length;i++){
+//     console.log(colors[i])
+//   }
+
+// 2. for in
+
+// for (i in colors) console.log(colors[i])
+
+
+
+//for of
+for (i in colors) console.log(colors[i])
+// for (i of colors) console.log(i)
+
+
+
+// 4. for each
+
+// colors.forEach(i=>console.log(i))
+
+// const colors2 = [1,2,3,4,5,6,7,8,9,10];
+// colors2.forEach(i=>console.log(i**2))
+
+// forEach() and for in skip empty elements in the array, for and for of do not. 
+
+
