11ebdcc89bf19e9f6789cd0c6c0334d7188a27d9
Matthew D <67597416+cwmatthew@users.noreply.github.com>
2021-11-04 22:18:56 +0300

    class start

--- javascript/class-notes/Session-08/index.js
+++ javascript/class-notes/Session-08/index.js
@@ -52,9 +52,67 @@ const person = {
   },
 };
 // console.log(person.getFullName2());
-
+/* 
 const print2 = person.printCitiesLived;
 // print();
 person.printCitiesLived();
 // console.log(this);
 // print2();
+
+function printThis() {
+  console.log(this);
+}
+printThis(); */
+
+// function Student() {
+//   this.name = 'John';
+//   this.grade = 68;
+//   this.printGrade = function () {
+//     return `${this.name} has a grade of ${this.grade}`;
+//   };
+//   this.printArrow = () => {
+//     return `${this.name} has a grade of ${this.grade}`;
+//   };
+// }
+// console.log(Student());
+
+// const student1 = new Student();
+// const student2 = new Student();
+// student2.name = 'Jane';
+
+// // console.log(student1.printGrade());
+// const pr = student1.printGrade;
+// console.log(pr.bind(student2)());
+
+// const arr = [1, 2, 3, 4, 5];
+// arr.push(8);
+// console.log(arr);
+
+function Animal(name, age) {
+  this.name = name;
+  this.age = age;
+  this.eat = function () {
+    console.log('nom nom nom');
+  };
+}
+
+function Cat(name, age) {
+  Animal.call(this, name, age);
+  this.color = 'black';
+  function speak() {
+    console.log('meow');
+  }
+}
+
+function Dog(name, age) {
+  Animal.call(this, name, age);
+  function speak() {
+    console.log('woof');
+  }
+}
+
+const cat1 = new Cat('Fluffy', 2);
+console.log(cat1);
+const karabas = new Dog('Karabaş', 3);
+karabas.prototype = Object.create(Animal.prototype);
+console.log(karabas);
