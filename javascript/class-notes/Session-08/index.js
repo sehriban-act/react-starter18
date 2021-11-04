56d246419329765f77a6d136acff687fd76b6635
Matthew D <67597416+cwmatthew@users.noreply.github.com>
2021-11-04 20:51:13 +0300

    obj2

--- javascript/class-notes/Session-08/index.js
+++ javascript/class-notes/Session-08/index.js
@@ -1,6 +1,6 @@
 console.log('» index.js is running');
 
-const person = {
+/* const person = {
   firstName: 'John',
   age: 30,
   location: {
@@ -22,4 +22,39 @@ console.log(person);
 console.log(person['2021']);
 
 const field = 'Name';
-console.log(person['first' + field]);
+console.log(person['first' + field]); */
+
+// methods
+
+const person = {
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 30,
+  citiesLived: ['New York', 'Paris', 'London'],
+  location: {
+    city: 'New York',
+    temp: 92,
+  },
+  2021: 'Clarusway',
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  getFullName2() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  aMethod: () => {
+    return 'Hello';
+  },
+  printCitiesLived() {
+    this.citiesLived.forEach(city => {
+      console.log(this.firstName + ' has lived in ' + city);
+    });
+  },
+};
+// console.log(person.getFullName2());
+
+const print2 = person.printCitiesLived;
+// print();
+person.printCitiesLived();
+// console.log(this);
+// print2();
