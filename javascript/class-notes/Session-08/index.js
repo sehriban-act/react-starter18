bc74e4d50e5472fe8ebce72fb72ffa81bc366bec
Matthew D <67597416+cwmatthew@users.noreply.github.com>
2021-11-04 20:23:59 +0300

    objects1

--- javascript/class-notes/Session-08/index.js
+++ javascript/class-notes/Session-08/index.js
@@ -1,1 +1,25 @@
 console.log('» index.js is running');
+
+const person = {
+  firstName: 'John',
+  age: 30,
+  location: {
+    city: 'New York',
+    temp: 92,
+  },
+  2021: 'Clarusway',
+};
+
+console.log(person.firstName);
+console.log(person['age']);
+console.log(person);
+console.log(person.lastName);
+
+person.lastName = 'Smith';
+console.log(person['midName']);
+
+console.log(person);
+console.log(person['2021']);
+
+const field = 'Name';
+console.log(person['first' + field]);
