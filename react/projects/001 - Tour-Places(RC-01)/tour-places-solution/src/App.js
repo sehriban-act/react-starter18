aa2fcbdc14d2ab10a42efb7097a6ed69bb932598
8490 <ethan@clarusway.com>
2021-12-04 16:47:12 +0300

    add cards.js

--- react/projects/001 - Tour-Places(RC-01)/tour-places-solution/src/App.js
+++ react/projects/001 - Tour-Places(RC-01)/tour-places-solution/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
+import Cards from "./components/cards/Cards";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Cards />
     </div>
   );
 }
