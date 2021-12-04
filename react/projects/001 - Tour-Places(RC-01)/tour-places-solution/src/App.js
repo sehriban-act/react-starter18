6f54207ea044555971e1bb1507e8c6f1c8ba45ae
8490 <ethan@clarusway.com>
2021-12-04 16:22:56 +0300

    add header

--- react/projects/001 - Tour-Places(RC-01)/tour-places-solution/src/App.js
+++ react/projects/001 - Tour-Places(RC-01)/tour-places-solution/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
