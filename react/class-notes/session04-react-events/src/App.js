ba24fce65251712f1cfb4a16637150f09e7a0928
felix <felixfranko66@gmail.com>
2021-12-07 20:28:10 +0300

    add addevents

--- react/class-notes/session04-react-events/src/App.js
+++ react/class-notes/session04-react-events/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import AddEventsClass from './components/addEventClass/AddEventsClass';
+import AddEvents from './components/addEvents/AddEvents';
 
 function App() {
   return (
     <div className="App">
       <h1>Events in React Js</h1>
+      {/* <AddEvents /> */}
+      <AddEventsClass />
     </div>
   );
 }
