747c758e6b61e1737fa012482ad07472828a325e
felix <felixfranko66@gmail.com>
2021-12-07 21:21:17 +0300

    v3

--- react/class-notes/session04-react-events/src/components/eventBinding/EventBind.js
+++ react/class-notes/session04-react-events/src/components/eventBinding/EventBind.js
@@ -1,10 +1,30 @@
 import { Component } from 'react';
 
 export default class EventBind extends Component {
-  handleClick() {
+  constructor(props) {
+    super(props);
+    this.state = { message: 'Hello' };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  //************* */
+  // handleClick() {
+  //   this.setState({ message: 'Goodbye' });
+  //   // alert('hello');
+  //   console.log(this);
+  // } //[1] [3]
+
+  //************* */
+  // handleClick(name) {
+  //   this.setState({ message: `Goodbye ${name}` });
+  // } //[2]
+
+  //*************** */
+  handleClick = () => {
+    this.setState({ message: 'Goodbye' });
     // alert('hello');
     console.log(this);
-  }
+  }; //[4]
   render() {
     return (
       <div>
@@ -14,9 +34,15 @@ export default class EventBind extends Component {
          * 3. Binding in the constructor
          * 4. Class property as an arrow function
          */}
-        <p></p>
+        <p>{this.state.message}</p>
         <h2>EventBinding</h2>
-        <button onClick={this.handleClick.bind(this)}>Click1</button>
+        <button onClick={this.handleClick}>Click4</button>
+        {/* <button onClick={this.handleClick}>Click3</button> */}
+        {/* [3] */}
+        {/* <button onClick={() => this.handleClick('Jane')}>Click1</button> */}
+        {/* [2] */}
+        {/* <button onClick={this.handleClick.bind(this)}>Click1</button> */}
+        {/* [1] */}
       </div>
     );
   }
