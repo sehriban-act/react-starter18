53dcf98723ae86e2f447831546b6cc9729cc5968
felix <felixfranko66@gmail.com>
2021-12-09 16:41:09 +0300

    add session06-basic-hooks

---
+++ react/class-notes/session06-basic-hooks/src/App.js
@@ -0,0 +1,16 @@
+// import './index.css'
+import ClassComponent from './components/ClassComponent'
+import FunctionalComponent from './components/FunctionalComponent'
+// import Users from './components/Users'
+
+function App() {
+  return (
+    <div className="App">
+      <ClassComponent />
+      <FunctionalComponent />
+      {/* <Users /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ react/class-notes/session06-basic-hooks/src/components/ClassComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ClassComponent extends React.Component {
+  render() {
+    return (
+      <div className="class">
+        <h2>Class Component</h2>
+      </div>
+    )
+  }
+}
+
+export default ClassComponent
