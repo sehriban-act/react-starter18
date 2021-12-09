b4c9e8b62b6a66036f7bbfce2ab1fca9e02c9b71
felix <felixfranko66@gmail.com>
2021-12-09 20:48:52 +0300

    v2

--- react/class-notes/session06-basic-hooks/src/components/FunctionalComponent.js
+++ react/class-notes/session06-basic-hooks/src/components/FunctionalComponent.js
@@ -3,11 +3,18 @@ import React, { useState } from 'react'
 const FunctionalComponent = () => {
   const [count, setCount] = useState(0)
   const [age, setAge] = useState(4)
+  const [user, setUser] = useState({ name: 'Felix', age: 25, email: 'felix@gmail.com' })
 
   // const increase = () => {
   //   setCount(count + 1)
   // }
   const increase = () => setCount(count + 1)
+
+  const increaseAge = () => setAge(age + 1)
+
+  const updateUser = () => {
+    setUser({ ...user, name: 'John' })
+  }
 
   return (
     <div className="function">
@@ -15,6 +22,12 @@ const FunctionalComponent = () => {
       <p>Count: {count}</p>
       {/* <button onClick={() => setCount(count + 1)}>Increase</button> */}
       <button onClick={increase}>Increase</button>
+      <p>Age : {age}</p>
+      <button onClick={increaseAge}>Increase Age</button>
+      <p>Name: {user.name} </p>
+      <p>Age: {user.age}</p>
+      <p>Email: {user.email}</p>
+      <button onClick={updateUser}>UpdateUser</button>
     </div>
   )
 }
