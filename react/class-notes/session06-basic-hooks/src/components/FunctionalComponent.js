bde5a07377d0f278d0e764d426094b199fe11945
felix <felixfranko66@gmail.com>
2021-12-09 20:31:01 +0300

    v2

--- react/class-notes/session06-basic-hooks/src/components/FunctionalComponent.js
+++ react/class-notes/session06-basic-hooks/src/components/FunctionalComponent.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FunctionalComponent = () => {
+  const [count, setCount] = useState(0)
+  const [age, setAge] = useState(4)
+
+  // const increase = () => {
+  //   setCount(count + 1)
+  // }
+  const increase = () => setCount(count + 1)
+
   return (
     <div className="function">
       <h2>Functional Component</h2>
+      <p>Count: {count}</p>
+      {/* <button onClick={() => setCount(count + 1)}>Increase</button> */}
+      <button onClick={increase}>Increase</button>
     </div>
   )
 }
