734c8b7ffd35b0805d1301f5c833b154d25d0ac8
felix <felixfranko66@gmail.com>
2021-12-09 22:25:32 +0300

    v7

--- react/class-notes/session06-basic-hooks/src/App.js
+++ react/class-notes/session06-basic-hooks/src/App.js
@@ -1,19 +1,19 @@
 // import './index.css'
 import { useState } from 'react'
-import ClassComponent from './components/ClassComponent'
-import FunctionalComponent from './components/FunctionalComponent'
-// import Users from './components/Users'
+// import ClassComponent from './components/ClassComponent'
+// import FunctionalComponent from './components/FunctionalComponent'
+import Users from './components/Users'
 
 function App() {
-  const [classComp, setClassComp] = useState(true)
-  const [funcComp, setFuncComp] = useState(true)
+  // const [classComp, setClassComp] = useState(true)
+  // const [funcComp, setFuncComp] = useState(true)
   return (
     <div className="App">
-      <button onClick={() => setClassComp(!classComp)}>Toggle Class Comp</button>
-      {classComp && <ClassComponent />}
-      <button onClick={() => setFuncComp(!funcComp)}>Toggle Func Comp</button>
-      {funcComp && <FunctionalComponent />}
-      {/* <Users /> */}
+      {/* <button onClick={() => setClassComp(!classComp)}>Toggle Class Comp</button> */}
+      {/* {classComp && <ClassComponent />} */}
+      {/* <button onClick={() => setFuncComp(!funcComp)}>Toggle Func Comp</button> */}
+      {/* {funcComp && <FunctionalComponent />} */}
+      <Users />
     </div>
   )
 }
--- react/class-notes/session06-basic-hooks/src/components/Users.js
+++ react/class-notes/session06-basic-hooks/src/components/Users.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Users = () => {
+  const [users, setUsers] = useState([])
+
+  const getUsers = () => {
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then((res) => res.json())
+    //   .then((json) => setUsers(json))
+    //   .catch((err) => console.log(err))
+  }
+  console.log(users)
   return (
     <div className="users">
       <h2>Users</h2>
+      <button onClick={getUsers}>Get Users</button>
+      <div className="users-cards">
+        {users.map((user) => (
+          <div className="useritem" key={user.id}>
+            <img src={`https://avatars.dicebear.com/v2/avataaars/${user.id}.svg`} alt="" />
+            {user.name}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
