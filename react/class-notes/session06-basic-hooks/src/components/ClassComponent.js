dd49a179645a2b6d50da565a98dcea45d45e6298
felix <felixfranko66@gmail.com>
2021-12-09 20:25:18 +0300

    v1

--- react/class-notes/session06-basic-hooks/src/components/ClassComponent.js
+++ react/class-notes/session06-basic-hooks/src/components/ClassComponent.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 class ClassComponent extends React.Component {
+  constructor() {
+    super()
+    this.state = { count: 0 }
+  }
+  increase = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
   render() {
     return (
       <div className="class">
         <h2>Class Component</h2>
+        <p>Count: {this.state.count} </p>
+        <button onClick={this.increase}>Increase</button>
       </div>
     )
   }
