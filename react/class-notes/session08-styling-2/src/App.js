3076d945f79e4792fb39f4494c20717660421fd8
felix <felixfranko66@gmail.com>
2021-12-13 22:28:34 +0300

    v8

--- react/class-notes/session08-styling-2/src/App.js
+++ react/class-notes/session08-styling-2/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Buttons from './components/material-ui/buttons/Buttons';
 import CheckBoxComp from './components/material-ui/checkbox/CheckBoxComp';
+import TextFieldComp from './components/material-ui/text-field/TextFieldComp';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           {/* <StyledComponents /> */}
+          <TextFieldComp />
           <CheckBoxComp />
           <Buttons />
         </header>
