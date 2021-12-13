b5a2bdeb43f2ebe9280e7ad67fd0c96df6b9b50e
felix <felixfranko66@gmail.com>
2021-12-13 20:46:37 +0300

    v3

--- react/class-notes/session08-styling-2/src/components/styled-comp/StyledComponents.js
+++ react/class-notes/session08-styling-2/src/components/styled-comp/StyledComponents.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button } from './button/Button.styles';
+import Wrapper from './wrapper/Wrapper';
 
 const StyledComponents = () => {
   return (
-    <>
+    <Wrapper bgColor="wheat">
       <Button primary>Primary</Button>
       <Button>Primary</Button>
-    </>
+    </Wrapper>
   );
 };
 
