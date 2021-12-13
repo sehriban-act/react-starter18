77fe15243f6e7a777c0916bd068ff5881999fe08
felix <felixfranko66@gmail.com>
2021-12-13 21:28:19 +0300

    v4

--- react/class-notes/session08-styling-2/src/components/styled-comp/StyledComponents.js
+++ react/class-notes/session08-styling-2/src/components/styled-comp/StyledComponents.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Button } from './button/Button.styles';
+import HeaderText from './header/HeaderText';
+import Link from './link/Link';
 import Wrapper from './wrapper/Wrapper';
 
 const StyledComponents = () => {
   return (
     <Wrapper bgColor="wheat">
+      <HeaderText>Clarusway</HeaderText>
+      <HeaderText headerColor="purple">Styled Components</HeaderText>
+      <Link
+        secondary
+        href="https://clarusway.com/"
+        target="_blank"
+        rel="noopener"
+      >
+        Learn FS Development
+      </Link>
       <Button primary>Primary</Button>
       <Button>Primary</Button>
     </Wrapper>
