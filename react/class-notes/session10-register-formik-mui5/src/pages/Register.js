5007ecac24095275702f2164b268de7609428765
felix <felixfranko66@gmail.com>
2021-12-18 17:23:09 +0300

    v1

--- react/class-notes/session10-register-formik-mui5/src/pages/Register.js
+++ react/class-notes/session10-register-formik-mui5/src/pages/Register.js
@@ -12,8 +12,15 @@ import {
   Typography,
 } from '@mui/material';
 import { blue } from '@mui/material/colors';
+import { Formik } from 'formik';
 
 function Register() {
+  const initialValues = {
+    username: 'felix',
+    email: '',
+    password: '',
+    password2: '',
+  };
   return (
     <Container
       sx={{
@@ -36,31 +43,65 @@ function Register() {
       <Typography sx={{ margin: '1rem' }} variant="h4">
         Sign Up
       </Typography>
-      <form>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <TextField name="username" label="User Name" variant="outlined" />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField name="email" label="Email" variant="outlined" />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField name="password" label="Password" />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              name="password2"
-              label="Password Again"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button type="submit" variant="contained" color="primary">
-              Register
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+      <Formik initialValues={initialValues}>
+        {(values, handleChange) => (
+          <form>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  name="username"
+                  label="User Name"
+                  variant="outlined"
+                  value={values.username}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  value={values.email}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  label="Password"
+                  type="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password2"
+                  label="Password Again"
+                  variant="outlined"
+                  type="password"
+                  value={values.password2}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Register
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
       <p>
         Already have an account?
         <Link
