98f56f44ca5ea169c11914150eb9c55528bed69e
edwardBenedict <edwardbenedict92@gmail.com>
2022-01-03 19:30:42 +0100

    fourth commit

---
+++ react/class-notes/session11-redux-01/src/redux/actions/todoActions.js
@@ -0,0 +1,34 @@
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  TOGGLE_TODO,
+  CLEAR_TODO_LIST,
+} from "../types/todoTypes";
+
+
+export const addTodo = (payload) => {
+    return {
+        type: ADD_TODO,
+        payload: payload
+    }
+}
+
+export const deleteTodo = (payload) => {
+    return {
+        type: DELETE_TODO,
+        payload
+    }
+}
+
+export const toggleTodo = () => {
+    return {
+        type: TOGGLE_TODO,
+        payload
+    }
+}
+
+export const clearTodoList = () =>{
+    return {
+        type: CLEAR_TODO_LIST
+    }
+}
--- react/class-notes/session11-redux-01/src/redux/index.js
+++ react/class-notes/session11-redux-01/src/redux/index.js
@@ -13,29 +13,3 @@ export const combineStore = () => {
     return store;
 }
 
-
-
-// const initialState = {
-//   counter: 0,
-// };
-
-// const reducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "INCREMENT":
-//       return {
-//         counter: state.counter + 1,
-//       };
-//     case "DECREMENT":
-//       return {
-//         counter: state.counter - 1,
-//       };
-//     case "RESET":
-//       return {
-//         counter: 0,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export default reducer;
