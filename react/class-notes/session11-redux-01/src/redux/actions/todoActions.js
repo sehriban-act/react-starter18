217326d210c4373d55e7df284cf1d5e56aaed327
edwardBenedict <edwardbenedict92@gmail.com>
2022-01-03 19:45:44 +0100

    fifth commit

--- react/class-notes/session11-redux-01/src/redux/actions/todoActions.js
+++ react/class-notes/session11-redux-01/src/redux/actions/todoActions.js
@@ -5,30 +5,29 @@ import {
   CLEAR_TODO_LIST,
 } from "../types/todoTypes";
 
-
 export const addTodo = (payload) => {
-    return {
-        type: ADD_TODO,
-        payload: payload
-    }
-}
+  return {
+    type: ADD_TODO,
+    payload: payload,
+  };
+};
 
 export const deleteTodo = (payload) => {
-    return {
-        type: DELETE_TODO,
-        payload
-    }
-}
+  return {
+    type: DELETE_TODO,
+    payload,
+  };
+};
 
-export const toggleTodo = () => {
-    return {
-        type: TOGGLE_TODO,
-        payload
-    }
-}
+export const toggleTodo = (payload) => {
+  return {
+    type: TOGGLE_TODO,
+    payload,
+  };
+};
 
-export const clearTodoList = () =>{
-    return {
-        type: CLEAR_TODO_LIST
-    }
-}
+export const clearTodoList = () => {
+  return {
+    type: CLEAR_TODO_LIST,
+  };
+};
