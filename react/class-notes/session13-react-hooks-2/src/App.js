fcaccefda5c172a3cad53d7687dcec70cf5331c7
felix <felixfranko66@gmail.com>
2022-01-06 13:46:04 +0300

    add session13 hooks-2 class notes

---
+++ react/class-notes/session13-react-hooks-2/src/App.js
@@ -0,0 +1,13 @@
+import UseRefComponent from './components/UseRefComponent';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <UseRefComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/class-notes/session13-react-hooks-2/src/components/UseRefComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UseRefComponent = () => {
+  return (
+    <div className="useref">
+      <h2>UseRef Component</h2>
+    </div>
+  );
+};
+
+export default UseRefComponent;
