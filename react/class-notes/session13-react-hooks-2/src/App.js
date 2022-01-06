88e933cc3a13e4bfb216047a961b58d57b60d256
felix <felixfranko66@gmail.com>
2022-01-06 21:22:46 +0300

    v3

--- react/class-notes/session13-react-hooks-2/src/App.js
+++ react/class-notes/session13-react-hooks-2/src/App.js
@@ -1,13 +1,25 @@
 // import UseRefComponent from './components/UseRefComponent';
+import fs from './assets/fs.png';
+import aws from './assets/aws.png';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [img, setImg] = useState(null);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       {/* <UseRefComponent /> */}
-      <Header />
+      <Header img={img} />
+      <p>Count: {count}</p>
+      <button onClick={increase}>Increase</button>
+      <button onClick={() => setImg(fs)}>FS</button>
+      <button onClick={() => setImg(aws)}>AWS</button>
     </div>
   );
 }
--- react/class-notes/session13-react-hooks-2/src/components/Header.js
+++ react/class-notes/session13-react-hooks-2/src/components/Header.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { memo } from 'react';
 import cw from '../assets/cw_logo.png';
 
-const Header = ({ img }) => {
+const Header = React.memo(({ img }) => {
   console.log('Rendering: Header Comp');
   return (
     <div className="header">
@@ -12,6 +12,36 @@ const Header = ({ img }) => {
       />
     </div>
   );
-};
+});
 
 export default Header;
+
+// const Header = ({ img }) => {
+//   console.log('Rendering: Header Comp');
+//   return (
+//     <div className="header">
+//       <img
+//         src={img ? img : cw}
+//         alt="CW_logo"
+//         style={{ margin: '1rem', maxHeight: '200px' }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default memo(Header);
+
+// const Header = ({ img }) => {
+//   console.log('Rendering: Header Comp');
+//   return (
+//     <div className="header">
+//       <img
+//         src={img ? img : cw}
+//         alt="CW_logo"
+//         style={{ margin: '1rem', maxHeight: '200px' }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default React.memo(Header);
